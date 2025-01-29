a0cec1778283325bf4c51b2fe2d0ded931c40891
C. Todd Lombardo <1851211+iamctodd@users.noreply.github.com>
2025-01-29 14:17:10 +0000

    updates before class

--- class.py
+++ class.py
@@ -1,17 +1,18 @@
-def main():
-    meow(get_number())
-
-
 def get_number():
     while True:
-        n = int(input("What's n? "))
-        if n > 0:
-            return n
-
+        try:
+            n = int(input("What's n? "))
+            if n > 0:
+                return n
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
 def meow(n):
     for _ in range(n):
         print("meow")
+
+def main():
+    meow(get_number())
 
 if __name__ == "__main__":
     main()
